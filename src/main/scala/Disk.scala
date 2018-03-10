e61718d2015b9eeaec95caaea995dcb342c06e30
Edward A Maxedon <edadma@gmail.com>
2018-03-10 10:29:08 -0500

    code basic ram disk

--- src/main/scala/Disk.scala
+++ src/main/scala/Disk.scala
@@ -1,9 +1,32 @@
 package xyz.hyperreal.riscv
 
 
-class Disk( cylinders: Int, heads: Int, sectors: Int, block: Int ) {
+abstract class Disk {
+  val cylinders: Int
+  val heads: Int
+  val sectors: Int
+  val block: Int
 
-  private val storage = Array.fill[Byte]( cylinders, heads, sectors, block )( 0 )
+  def format: Unit
 
+  def read( c: Int, h: Int, s: Int ): Vector[Byte]
+
+  def write( c: Int, h: Int, s: Int, b: Seq[Byte] ): Unit
+}
+
+class RamDisk( val cylinders: Int, val heads: Int, val sectors: Int, val block: Int ) extends Disk {
+
+  private var storage: Array[Array[Array[Array[Byte]]]] = _
+
+  def format: Unit = {
+    storage = Array.fill[Byte]( cylinders, heads, sectors, block )( 0 )
+  }
+
+  def read( c: Int, h: Int, s: Int ) = storage(c)(h)(s).toVector
+
+  def write( c: Int, h: Int, s: Int, b: Seq[Byte] ): Unit = {
+    for ((d, i) <- b zipWithIndex)
+      storage(c)(h)(s)(i) = d
+  }
 
 }
