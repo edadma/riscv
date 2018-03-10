fbcc084457e982a4ef4ddedbb977a51956ab4d2a
Edward A Maxedon <edadma@gmail.com>
2018-03-10 13:28:54 -0500

    code basic file disk

--- src/main/scala/Disk.scala
+++ src/main/scala/Disk.scala
@@ -1,5 +1,7 @@
 package xyz.hyperreal.riscv
 
+import java.io.RandomAccessFile
+
 
 abstract class Disk {
   val cylinders: Int
@@ -27,6 +29,35 @@ class RamDisk( val cylinders: Int, val heads: Int, val sectors: Int, val block: 
   def write( c: Int, h: Int, s: Int, b: Seq[Byte] ): Unit = {
     for ((d, i) <- b zipWithIndex)
       storage(c)(h)(s)(i) = d
+  }
+
+}
+
+class FileDisk( pathname: String, val cylinders: Int, val heads: Int, val sectors: Int, val block: Int ) extends Disk {
+
+  private val file = new RandomAccessFile( pathname, "rw" )
+
+  if (file.length == 0)
+    format
+
+  override def format: Unit = {
+    val b = new Array[Byte]( block )
+
+    for (_ <- cylinders*heads*sectors)
+      file.write( block )
+  }
+
+  override def read(c: Int, h: Int, s: Int): Vector[Byte] = {
+    val b = new Array[Byte]( block )
+
+    file.seek( c*heads*sectors*block + h*sectors*block + s*block )
+    file.readFully( b )
+    b.toVector
+  }
+
+  override def write(c: Int, h: Int, s: Int, b: Seq[Byte]): Unit = {
+    file.seek( c*heads*sectors*block + h*sectors*block + s*block )
+    file.write( b.toArray )
   }
 
 }
